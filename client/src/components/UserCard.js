481b631821be5c2ffc3533c7db3275d530b26359
Jaakko <jaakko.lipponen01@gmail.com>
2024-02-21 14:25:51 +0200

    Update UserCard component and User model

--- client/src/components/UserCard.js
+++ client/src/components/UserCard.js
@@ -3,11 +3,16 @@ import { Card } from "react-bootstrap";
 
 function UserCard({ username, bio }) {
   return (
-    <Card style={{ width: "18rem" }} className="mb-2">
+    <Card style={{ width: "18rem", height: "18rem" }} className="mb-2">
       <Card.Header>{username}</Card.Header>
       <Card.Body>
-        <Card.Title>Hi, I like hedgehogs!</Card.Title>
-        <Card.Text>{bio}</Card.Text>
+        <Card.Title>{bio.split(/[\n\s]/)[0]}</Card.Title>
+        <Card.Text>
+          {bio
+            .split(/[\n\s]/)
+            .slice(1)
+            .join(" ")}
+        </Card.Text>
       </Card.Body>
     </Card>
   );
--- server/models/User.js
+++ server/models/User.js
@@ -4,7 +4,7 @@ const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   description: { type: String },
-  matches: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+  likes: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
 });
 
 module.exports = mongoose.model("User", userSchema);
